368caa228fc99c108d592fda9e12ddf45759b8e8
ZaaRah . <rheza.anugrah00@gmail.com>
2024-06-20 20:41:07 +0700

    commit

--- Form1.cs
+++ Form1.cs
@@ -13,49 +13,13 @@ namespace CahyaFy
 
         private void InitializeCustomComponents()
         {
-            // Add event handlers for any necessary events
+            // Add event handlers if necessary
             this.Load += new EventHandler(Form1_Load);
-            this.txtKasirNo.TextChanged += new EventHandler(txtKasirNo_TextChanged);
-            this.txtKasir.TextChanged += new EventHandler(txtKasir_TextChanged);
-            this.txtNotaNo.TextChanged += new EventHandler(txtNotaNo_TextChanged);
-            this.txtTanggal.TextChanged += new EventHandler(txtTanggal_TextChanged);
-            this.txtNilaiTerhemat.TextChanged += new EventHandler(txtNilaiTerhemat_TextChanged);
-            this.txtStokTerakhir.TextChanged += new EventHandler(txtStokTerakhir_TextChanged);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // Load initial data or settings
-        }
-
-        private void txtKasirNo_TextChanged(object sender, EventArgs e)
-        {
-            // Handle text change event for txtKasirNo
-        }
-
-        private void txtKasir_TextChanged(object sender, EventArgs e)
-        {
-            // Handle text change event for txtKasir
-        }
-
-        private void txtNotaNo_TextChanged(object sender, EventArgs e)
-        {
-            // Handle text change event for txtNotaNo
-        }
-
-        private void txtTanggal_TextChanged(object sender, EventArgs e)
-        {
-            // Handle text change event for txtTanggal
-        }
-
-        private void txtNilaiTerhemat_TextChanged(object sender, EventArgs e)
-        {
-            // Handle text change event for txtNilaiTerhemat
-        }
-
-        private void txtStokTerakhir_TextChanged(object sender, EventArgs e)
-        {
-            // Handle text change event for txtStokTerakhir
+            // Load initial data or settings if needed
         }
     }
 }
